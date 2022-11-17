ecfce74c9f9110bdaebd9061a37eaeec8cb0c140
simdum25 <simon.dumas1@uqac.ca>
2022-11-17 10:32:52 -0500

    Ajout interface login

---
+++ app/src/main/java/uqac/dim/projet_gestion/Control/Login.java
@@ -0,0 +1,25 @@
+package uqac.dim.projet_gestion.Control;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import uqac.dim.projet_gestion.R;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/uqac/dim/projet_gestion/Control/MainActivity.java
+++ app/src/main/java/uqac/dim/projet_gestion/Control/MainActivity.java
@@ -5,11 +5,13 @@ import uqac.dim.projet_gestion.Model.*;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.KeyListener;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
@@ -70,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     protected void addEmploye(View view)
     {
